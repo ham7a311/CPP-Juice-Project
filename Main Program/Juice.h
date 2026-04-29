cc9c961264796196c6167147d101710d7002c200
Hamza Al-Bulushi <184682032+ham7a311@users.noreply.github.com>
2026-04-29 10:45:49 +0400

    Add Juice class with price calculation method
    Implemented Juice class with price calculation based on size.

---
+++ Main Program/Juice.h
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Juice {
+    private:
+        string juiceName;
+        double basePrice, price;
+        char size;
+
+    public:
+        Juice(string juiceName, double basePrice, char size) {
+            this->basePrice = basePrice;
+            this->juiceName = juiceName;
+            this->size = size;
+        }
+
+        double calculatePrice() {
+            if(size == 'S' || size == 's') {
+                price = basePrice;
+            }else if (size == 'M' || size == 'm') {
+                price = basePrice + (basePrice * 0.5);
+            }else {
+                price = basePrice + basePrice;
+            }
+
+            return price;
+        }
+
+};
+
+
+int main() {
+    return 0;
+}
