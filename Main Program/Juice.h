a344db27d7a5ba002b56d6b8725f5d632c7ba3ae
Mahmood Al Busaidi <160405585+ItzMahmood@users.noreply.github.com>
2026-05-12 21:47:40 +0400

    Modification of Juice.h & Menu.h

--- Main Program/Juice.h
+++ Main Program/Juice.h
@@ -11,13 +11,21 @@ class Juice {
         char size;      // size of juice (S/M/L or s/m/l)
 
     public:
+        //Constructor
         Juice(string juiceName, double basePrice, char size) {
             this->basePrice = basePrice;
             this->juiceName = juiceName;
             this->size = size;
         }
 
-        double calculatePrice() {
+
+        // Juice name getter (Needed in Menu.h)
+        string getJuiceName() const {     // const because it only return the value of juiceName not changing it
+            return juiceName;
+        }
+
+        // A function to set the basePrice based on the size of the juice.
+        double calculatePrice() const  {            // const because it does not change any data member, it only reads the basePrice and returns the price after the modifications.
             if(size == 'S' || size == 's') {
                 return basePrice;                               //if the cup size is SMALL, price will be same, return basePrice
             }else if (size == 'M' || size == 'm') {  
@@ -37,16 +45,20 @@ class Juice {
                 return -1;                                 
             }
         }
-        void displayJuiceInfo(){
+
+
+        // A function to display the Info. of the Juice.
+        void displayJuiceInfo() const{          // const because it only displays the Info.
 
             double finalPrice = calculatePrice();           // call caculatePrice() to get the price variable and assign it to finalPrice
             
-            if(finalPrice == -1) cout << "Something went wrong (Invalid size)";
+            if(finalPrice == -1) cout << "Something went wrong (Invalid size)" << endl;
             else {
-                    cout << "Juice name: " << this->juiceName << endl;        // display juiceName
-                    cout << "Size: " << this->size << endl;            // display juice size
+                    cout << "--------------- Juice Informations --------------" << endl;
+                    cout << "Juice name: " << juiceName << endl;        // display juiceName
+                    cout << "Size: " << size << endl;            // display juice size
                     cout << "Price: " << finalPrice << endl;            // display finalPrice (price after adjusting and checking size)
-            }
+            }       cout << "-------------------------------------------------" << endl;
             
         };
 
---
+++ Main Program/Menu.h
@@ -0,0 +1,59 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include<iostream>
+#include<vector>        //better to use instead of an array so we can add and delete juices without any problems
+#include"Juice.h"
+using namespace std;
+
+// Menu class to manage a collection of juice objs available for sale.
+class Menu{
+    private:
+        vector<Juice> availableJuice;          //Dynamic list to store the different juice objects.
+
+    public:
+        // A fucntion to print the menu
+        void showMenu() const{               // const for two reasons: 1) displayJuiceInfo is const in Jucie.h so to use it here ShowMenu must be const. 2) the function it self only print Info. about the Juice, not changing any thing.
+            cout << "Available Jucies:" << endl;
+            for(int i = 0; i < availableJuice.size(); i++){
+                availableJuice[i].displayJuiceInfo();       //goes through the list and calls the display function for each juice
+                cout << endl;
+            }
+        }
+
+
+
+        // A function to add a new juice to the menu
+        void addJuice(Juice j){
+            availableJuice.push_back(j);   //Adds a new juice object to the end of the availableJuices list.
+        }
+
+
+        //A function to remove a juice based on its name.
+        void removeJuice(string name){
+            for(int i = 0; i < availableJuice.size(); i++){
+                if(availableJuice[i].getJuiceName() == name){
+                    availableJuice.erase(availableJuice.begin() + i);
+                    cout << "Jucie deleted." << endl; 
+                    return;
+                }
+            }
+            cout << "Juice not found!!" << endl;
+        }
+
+
+        void searchJucie(string name){
+            for(int i = 0; i < availableJuice.size(); i++){
+                if(availableJuice[i].getJuiceName() == name){
+                    cout << "Juice found." << endl; 
+                    availableJuice[i].displayJuiceInfo();
+                    return;
+                }
+            }
+            cout << "Juice not found!!" << endl;
+        }
+};
+
+
+
+#endif
