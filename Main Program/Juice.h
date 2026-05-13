44b182c0c2a8cd762252aa554b399b6c67fa9c07
Hamza Al-Bulushi <184682032+ham7a311@users.noreply.github.com>
2026-05-14 03:26:09 +0400

    Refactor Juice class constructor and price calculation
    Removed size parameter from Juice constructor and updated calculatePrice method to accept size as an argument.
    
    it would not be realistic to pass size in the constructor
    
    because we would be doing this:
            menu.addJuice(Juice("Mango", 1, 'S'))
            menu.addJuice(Juice("Mango", 1, 's'))
            menu.addJuice(Juice("Mango", 1, 'M'))
            menu.addJuice(Juice("Mango", 1, 'm'))
            menu.addJuice(Juice("Mango", 1, 'L'))
            menu.addJuice(Juice("Mango", 1, 'l'))
    and repeat the process for every single juice, that would be inefficient.
    
    Instead, we can do:
            menu.addJuice(Juice("Mango, 5))
    once and this will include all sizes, then calculate the price after user chooses size in the Order class constructor.

--- Main Program/Juice.h
+++ Main Program/Juice.h
@@ -8,18 +8,17 @@ class Juice {
     private:
         string juiceName;           // "Mango", "Strawberry", "Banana", "Pineapple", "Mint", "Ice"
         double basePrice;    // basePrice -> the starting price for every juice(this is decided in menu)
-        char size;      // size of juice (S/M/L or s/m/l)
 
     public:
         //Constructor
-        Juice(string juiceName, double basePrice, char size);
+        Juice(string juiceName, double basePrice);
 
 
         // Juice name getter (Needed in Menu.h)
         string getJuiceName() const;// const because it only return the value of juiceName not changing it
 
         // A function to set the basePrice based on the size of the juice.
-        double calculatePrice() const; // const because it does not change any data member, it only reads the basePrice and returns the price after the modifications.
+        double calculatePrice(char size) const; // const because it does not change any data member, it only reads the basePrice and returns the price after the modifications.
 
 
         // A function to display the Info. of the Juice.
