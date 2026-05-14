c257d395f9f9e903918b26ffe904c8b4ef98237c
Hamza Al-Bulushi <184682032+ham7a311@users.noreply.github.com>
2026-05-14 16:07:58 +0400

    Refactor Order class methods for clarity and efficiency

--- Main Program/Order.cpp
+++ Main Program/Order.cpp
@@ -5,23 +5,24 @@ Order::Order(Customer c) {
     customer = c;
     totalPrice = 0;
     status = "Pending";
-    orderID = counter++;      // increase the counter everytime a new order is created
+    orderID = counter++;      // increament the counter everytime a new order is created
     
 }
 
 
 // size is the CUP size not Array/Vector size
 void Order::addJuiceToOrder(Juice j, char size) {
-    juices.push_back(make_pair(j, size));
-    sizes.push_back(size);
+    juices.push_back(make_pair(j, size));    // push back j and size as an array since they are more than one element(pair), you can use make_pair or {}
 }
 
 
 double Order::calculateTotal() {
-    totalPrice = 0;        // reset totalPrice everytime the function is called to avoid other orders' prices clashing with each other and result in unexpected output
-
-    for (int i = 0; i < juices.size(); i++) {
-        totalPrice += juices[i].calculatePrice(sizes[i]);
+    totalPrice = 0;               // reset totalPrice everytime the function is called to avoid other orders' prices clashing with each other and result in unexpected output
+    // auto helps you with writing the type instead of you writing it manually, without auto, we'll have to write for (pair<Juice, char> obj : juices) {...}
+    for(auto obj: juices) {        
+        auto juice = obj.first;
+        auto size = obj.second;
+        totalPrice += juice.calculatePrice(size);    // item.first --> Juice, item.second --> size(S/M/L or s/m/l)
     }
 
     return totalPrice;
@@ -35,10 +36,12 @@ void Order::showOrderDetails() {
 
     cout << "Your Order:" << endl;
 
-    for (int i = 0; i < juices.size(); i++) {
-        cout << "Juice: " << juices[i].getJuiceName() << endl;
-        cout << "Size: " << sizes[i] << endl;
-        cout << "Price: " << juices[i].calculatePrice(sizes[i]) << endl;
+    for (auto obj : juices) {
+        auto juice = obj.first;
+        auto size = obj.second;
+        cout << "Juice: " << juice.getJuiceName() << endl;
+        cout << "Size: " << size << endl;
+        cout << "Price: " << juice.calculatePrice(size) << endl;
     }
 
     cout << "Total Price: " << calculateTotal() << endl;
