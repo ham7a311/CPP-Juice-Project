6f3ef6b6142cd367d1dd0a99d1491ee0642cf353
ItzMahmood <busaidi360b@gmail.com>
2026-05-17 16:49:17 +0400

    Menu.cpp

---
+++ Main Program/Menu.cpp
@@ -0,0 +1,43 @@
+#include "Menu.h"
+
+
+void Menu::showMenu() const{
+    cout << "Available Juices:" << endl;
+            for(int i = 0; i < availableJuice.size(); i++){
+                availableJuice[i].displayJuiceInfo();  
+                cout << endl;
+            }
+}
+
+
+
+void Menu::addJuice(Juice j){
+    availableJuice.push_back(j);
+}
+
+
+
+void Menu::removeJuice(string name){
+    for(int i = 0; i < availableJuice.size(); i++){
+                if(availableJuice[i].getJuiceName() == name){
+                    availableJuice.erase(availableJuice.begin() + i);
+                    cout << "Juice deleted." << endl; 
+                    return;
+                }
+    }
+    cout << "Juice not found!!" << endl;
+}
+
+
+
+
+void Menu::searchJuice(string name){
+    for(int i = 0; i < availableJuice.size(); i++){
+                if(availableJuice[i].getJuiceName() == name){
+                    cout << "Juice found." << endl; 
+                    availableJuice[i].displayJuiceInfo();
+                    return;
+                }
+    }
+    cout << "Juice not found!!" << endl;
+}
--- Main Program/Menu.h
+++ Main Program/Menu.h
@@ -13,45 +13,21 @@ class Menu{
 
     public:
         // A fucntion to print the menu
-        void showMenu() const{               // const for two reasons: 1) displayJuiceInfo is const in Jucie.h so to use it here ShowMenu must be const. 2) the function it self only print Info. about the Juice, not changing any thing.
-            cout << "Available Jucies:" << endl;
-            for(int i = 0; i < availableJuice.size(); i++){
-                availableJuice[i].displayJuiceInfo();       //goes through the list and calls the display function for each juice
-                cout << endl;
-            }
-        }
+        void showMenu() const;             // const for two reasons: 1) displayJuiceInfo is const in Jucie.h so to use it here ShowMenu must be const. 2) the function it self only print Info. about the Juice, not changing any thing.
+
 
 
 
         // A function to add a new juice to the menu
-        void addJuice(Juice j){
-            availableJuice.push_back(j);   //Adds a new juice object to the end of the availableJuices list.
-        }
+        void addJuice(Juice j);   //Adds a new juice object to the end of the availableJuices list.
+
 
 
         //A function to remove a juice based on its name.
-        void removeJuice(string name){
-            for(int i = 0; i < availableJuice.size(); i++){
-                if(availableJuice[i].getJuiceName() == name){
-                    availableJuice.erase(availableJuice.begin() + i);
-                    cout << "Jucie deleted." << endl; 
-                    return;
-                }
-            }
-            cout << "Juice not found!!" << endl;
-        }
+        void removeJuice(string name);
 
 
-        void searchJuice(string name){
-            for(int i = 0; i < availableJuice.size(); i++){
-                if(availableJuice[i].getJuiceName() == name){
-                    cout << "Juice found." << endl; 
-                    availableJuice[i].displayJuiceInfo();
-                    return;
-                }
-            }
-            cout << "Juice not found!!" << endl;
-        }
+        void searchJuice(string name);
 };
 
 
