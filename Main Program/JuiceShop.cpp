541b8a8ed0f8b8a88cbd902b5586fc3fb0ff5f67
Taif Albadi <tarmy797@gmail.com>
2026-05-17 22:05:37 +0400

    Update JuiceShop.cpp

--- Main Program/JuiceShop.cpp
+++ Main Program/JuiceShop.cpp
@@ -5,3 +5,8 @@ Menu& JuiceShop::getMenu(){ //Getting the menu using Encapsulation
 void JuiceShop::addEmployee(Employee emp){
   employees.push_back(emp); //Adding a new employee to the employees vector
 }
+void JuiceShop::CreateOrder(Order ord){
+   orders.push_back(ord); //Adding a new order to the orders vector
+}
+void JuiceShop::DisplayOrders(){
+   
