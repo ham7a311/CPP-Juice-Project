bbe84539a852ad3c385745faae6aefe72eea8fc0
Taif Albadi <tarmy797@gmail.com>
2026-05-18 19:22:04 +0400

    Update JuiceShop.h

--- Main Program/JuiceShop.h
+++ Main Program/JuiceShop.h
@@ -1,2 +1,22 @@
 #ifndef JUICESHOP_H
 #define JUICESHOP_H
+#include<iostream>
+#include<vector>
+#include"Menu.h"
+#include"Employee.h"
+#include"Order.h"
+using namespace std;
+class JuiceShop{
+  private:
+     Menu menu;
+     vector<Employee> employees;
+     vector<Order> orders;
+  public:
+      Menu& getMenu();
+      void addEmployee(Employee emp);
+      void createOrder(Order ord);
+      void displayOrders()const;
+      void checkOrder(int id)const;
+      void checkoutOrder(int id);
+};
+#endif
