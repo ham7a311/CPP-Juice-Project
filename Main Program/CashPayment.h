bf505beb3cb92e10c030dd0a79b119077d756027
Ali Al-Harthy <aliharthy5@gmail.com>
2026-05-23 11:01:13 +0400

    Update CashPayment.h

--- Main Program/CashPayment.h
+++ Main Program/CashPayment.h
@@ -1,1 +1,13 @@
+#ifndef CASHPAYMENT_H
+#define CASHPAYMENT_H
 
+#include "Payment.h"
+
+class CashPayment : public Payment {
+public:
+    CashPayment(double a);
+
+    void processPayment() override;
+};
+
+#endif
