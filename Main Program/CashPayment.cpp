89de651b7831f85ed91ae3a38e636d065f781a89
Ali Al-Harthy <aliharthy5@gmail.com>
2026-05-24 14:48:46 +0400

    Update CashPayment.cpp I updated the CashPayment class to make the cash payment more realistic.  Now the customer enters the amount of cash they paid, and the program compares it with the bill amount.

--- Main Program/CashPayment.cpp
+++ Main Program/CashPayment.cpp
@@ -1,18 +1,40 @@
-#ifndef CASHPAYMENT_H
-#define CASHPAYMENT_H
+#include "CashPayment.h"
 
-#include "Payment.h"
+// constructor
+CashPayment::CashPayment(double a, double cash)
+    : Payment(a, "Cash") {
 
-class CashPayment : public Payment {
-private:
-    double cashGiven;
+    if (cash < 0) {
+        throw invalid_argument("Cash amount cannot be negative.");
+    }
 
-public:
-    CashPayment(double a, double cash);
+    cashGiven = cash;
+}
 
-    void processPayment() override;
+// calculate the remaining change
+double CashPayment::calculateChange() const {
+    if (cashGiven > amount) {
+        return cashGiven - amount;
+    }
+    else if (cashGiven == amount) {
+        return 0;
+    }
+    else {
+        return -1;
+    }
+}
 
-    double calculateChange() const;
-};
+// cash payment process
+void CashPayment::processPayment() {
+    double change = calculateChange();
 
-#endif
+    if (change == -1) {
+        cout << "Cash payment unsuccessful. Not enough cash." << endl;
+    }
+    else {
+        cout << "Cash payment successful." << endl;
+        cout << "Bill amount : " << amount << " OMR" << endl;
+        cout << "Cash given  : " << cashGiven << " OMR" << endl;
+        cout << "Change      : " << change << " OMR" << endl;
+    }
+}
