a70545189c40fdf5e3363be3dea9d4870b009f5f
Hamza Al-Bulushi <184682032+ham7a311@users.noreply.github.com>
2026-05-24 06:16:08 +0400

    Make displayPersonInfo a pure virtual function
    Changed displayPersonInfo to a pure virtual function to enforce subclass implementation.

--- Main Program/Person.h
+++ Main Program/Person.h
@@ -24,8 +24,9 @@ class Person{
         void setPhoneNumber(string phone);
 
 
-        //display common person Info.
-        virtual void displayPersonInfo() const;
+        // pure virtual(function = 0) to make it abstract, this function has to be overriden by another function in Cusstomer and Employee.
+        // add '= 0' to make it not possible to create a seperate object from this class, because a Person is either a Customer and Employee, and you cant create a Person("Ahmed") or Person("Ahmed", "99483902" for example
+        virtual void displayPersonInfo() const = 0;
 };
 
 
